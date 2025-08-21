da93cf2319a5da8cbbe90bb759f379f94f934a86
Arthur <arthurhenrrique1705@gmail.com>
2025-08-21 13:37:29 -0300

    Preparando estruturas de pastas

--- TCC/src/main/java/com/senai/TCC/TccApplication.java
+++ TCC/src/main/java/com/senai/TCC/TccApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TccApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TccApplication.class, args);
 	}
