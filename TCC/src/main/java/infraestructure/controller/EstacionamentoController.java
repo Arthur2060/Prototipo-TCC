25c6d458afd4352eb1bb7d6553d25ac88ea9bb09
Arthur <arthurhenrrique1705@gmail.com>
2025-08-21 17:15:57 -0300

    Começando controller de estacionamento

---
+++ TCC/src/main/java/infraestructure/controller/EstacionamentoController.java
@@ -0,0 +1,26 @@
+package infraestructure.controller;
+
+import application.dtos.EstacionamentoDTO;
+import application.services.EstacionamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/estacionamento")
+public class EstacionamentoController {
+    @Autowired
+    private EstacionamentoService service;
+
+    @GetMapping
+    public ResponseEntity<List<EstacionamentoDTO>> listarEstacionamentos() {
+        return ResponseEntity.ok(service.listarTodosOsEstacionamentos());
+    }
+
+    @PostMapping("/{id}")
+    private ResponseEntity<EstacionamentoDTO> atualizarEstacionamento(@RequestBody EstacionamentoDTO dto, @PathVariable Long id) {
+        return ResponseEntity.ok(service.cadastrarEstacionamento(dto, id));
+    }
+}
