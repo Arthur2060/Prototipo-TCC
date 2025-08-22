da47591bcda7d1a6299c22404378f8d9dd07a949
Arthur <arthurhenrrique1705@gmail.com>
2025-08-22 08:27:44 -0300

    Documentando controller de Dono e DTOs de dono e usuario base

--- TCC/src/main/java/application/dtos/usuarioDTO/DonoDTO.java
+++ TCC/src/main/java/application/dtos/usuarioDTO/DonoDTO.java
@@ -1,13 +1,38 @@
 package application.dtos.usuarioDTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import model.entities.usuarios.DonoEstacionamento;
 
 import java.util.Date;
 
 public record DonoDTO(
+        @Schema(
+                name = "Nome",
+                description = "Nome do usuario.",
+                examples = "Pedro"
+        )
         String nome,
+
+        @Schema(
+                name = "email",
+                description = "E-mail do usuario",
+                examples = "pedro.nascimento@gmail.com"
+        )
         String email,
+
+        @Schema(
+                name = "Senha",
+                description = "Senha de acesso do usuario, " +
+                        "deve conter pelo menos uma letra maiuscula, simbolo e numero",
+                examples = "Estacio_2025"
+        )
         String senha,
+
+        @Schema(
+                name = "Data de nascimento",
+                description = "Data de nascimento do usuario, deve ser maior de 18 anos",
+                examples = "18-09-2000"
+        )
         Date dataNascimento
 ) {
     public DonoEstacionamento fromDTO() {
--- TCC/src/main/java/application/dtos/usuarioDTO/UsuarioDTO.java
+++ TCC/src/main/java/application/dtos/usuarioDTO/UsuarioDTO.java
@@ -1,5 +1,6 @@
 package application.dtos.usuarioDTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import model.entities.usuarios.Cliente;
 import model.entities.usuarios.DonoEstacionamento;
 import model.entities.usuarios.Gerente;
@@ -10,10 +11,41 @@ import model.exceptions.TipoDeUsuarioInvalido;
 import java.util.Date;
 
 public record UsuarioDTO(
+        @Schema(
+                name = "Nome",
+                description = "Nome do usuario.",
+                examples = "Pedro"
+        )
         String nome,
+
+        @Schema(
+                name = "email",
+                description = "E-mail do usuario",
+                examples = "pedro.nascimento@gmail.com"
+        )
         String email,
+
+        @Schema(
+                name = "Senha",
+                description = "Senha de acesso do usuario, " +
+                        "deve conter pelo menos uma letra maiuscula, simbolo e numero",
+                examples = "Estacio_2025"
+        )
         String senha,
+
+        @Schema(
+                name = "Data de nascimento",
+                description = "Data de nascimento do usuario, deve ser maior de 18 anos",
+                examples = "18-09-2000"
+        )
         Date dataNascimento,
+
+        @Schema(
+                name = "Tipo de usuario",
+                description = "Um dos quatro tipos de usuario possiveis:" +
+                        "CLIENTE, GERENTE ou DONO.",
+                examples = "CLENTE"
+        )
         TipoDeUsuario tipoDeUsuario
 ) {
     public Usuario fromDto() {
