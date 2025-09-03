6f28bc42ec92d29cde9f14a850513cf10fca3e21
Arthur <arthurhenrrique1705@gmail.com>
2025-09-02 21:05:58 -0300

    Retirando imports inutilizados de AutoWired

--- TCC/src/main/java/com/senai/TCC/infraestructure_ui/controller/usuario/DonoController.java
+++ TCC/src/main/java/com/senai/TCC/infraestructure_ui/controller/usuario/DonoController.java
@@ -4,7 +4,6 @@ import com.senai.TCC.application.dtos.usuarioDTO.DonoDTO;
 import com.senai.TCC.application.services.usuario.DonoService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
